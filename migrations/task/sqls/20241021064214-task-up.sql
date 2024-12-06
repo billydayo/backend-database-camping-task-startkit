c6488d4d8fc8161b797609dd5f950ea774eda9c6
billydayo <billyfang56@gmail.com>
2024-12-06 22:00:04 +0800

    5-5

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -234,20 +234,9 @@ where user_id = (select id from "USER" where email = 'wXlTq@hexschooltest.io')
 and course_id = (select id from "COURSE" where user_id = (select id from "USER" where email = 'lee2000@hexschooltest.io')) 
 and status = '即將授課';
 -- 5-6. 查詢：計算用戶王小明的購買堂數，顯示須包含以下欄位： user_id , total。 (需使用到 SUM 函式與 Group By)
-select 
-"CREDIT_PURCHASE".user_id, 
-sum("CREDIT_PURCHASE".purchased_credits) as total
-  from "CREDIT_PURCHASE" 
-  where "CREDIT_PURCHASE".user_id = (select id from "USER" where email = 'wXlTq@hexschooltest.io')
-  group by "CREDIT_PURCHASE".user_id;
+
 -- 5-7. 查詢：計算用戶王小明的已使用堂數，顯示須包含以下欄位： user_id , total。 (需使用到 Count 函式與 Group By)
-select 
-  "COURSE_BOOKING".user_id, 
-count(*) as total
-from "COURSE_BOOKING" 
-where "COURSE_BOOKING".user_id = (select id from "USER" where email = 'wXlTq@hexschooltest.io')
-  and status != '課程已取消'
-group by "COURSE_BOOKING".user_id;
+
 -- 5-8. [挑戰題] 查詢：請在一次查詢中，計算用戶王小明的剩餘可用堂數，顯示須包含以下欄位： user_id , remaining_credit
     -- 提示：
     -- select ("CREDIT_PURCHASE".total_credit - "COURSE_BOOKING".used_credit) as remaining_credit, ...
@@ -265,45 +254,15 @@ group by "COURSE_BOOKING".user_id;
 -- 6. 後台報表
 -- 6-1 查詢：查詢專長為重訓的教練，並按經驗年數排序，由資深到資淺（需使用 inner join 與 order by 語法)
 -- 顯示須包含以下欄位： 教練名稱 , 經驗年數, 專長名稱
-select 
-  "USER"."name" as "教練名稱",
-  "COACH".experience_years as "經驗年數", 
-  "SKILL".name as "專長名稱" 
-from "COACH_LINK_SKILL"
-inner JOIN "SKILL" on "SKILL".id = "COACH_LINK_SKILL".skill_id
-inner JOIN "COACH" on "COACH".id = "COACH_LINK_SKILL".coach_id
-inner JOIN "USER" on "USER".id = "COACH".user_id
-where "COACH_LINK_SKILL".skill_id = (select id from "SKILL" where name = '重訓')   
-order by "COACH".experience_years desc;
+
 -- 6-2 查詢：查詢每種專長的教練數量，並只列出教練數量最多的專長（需使用 group by, inner join 與 order by 與 limit 語法）
 -- 顯示須包含以下欄位： 專長名稱, coach_total
-select 
-  "SKILL".name as "專長名稱",
-  count(*) as coach_total
-from "COACH_LINK_SKILL"
-inner JOIN "SKILL" on "SKILL".id = "COACH_LINK_SKILL".skill_id
-group by "SKILL".name 
-order by coach_total desc 
-limit 1;
+
 -- 6-3. 查詢：計算 11 月份組合包方案的銷售數量
 -- 顯示須包含以下欄位： 組合包方案名稱, 銷售數量
-select  
-  "CREDIT_PACKAGE".name as "組合包方案名稱",
-  count(*) as "銷售數量"
-from "CREDIT_PURCHASE"
-inner join "CREDIT_PACKAGE" on "CREDIT_PACKAGE".id = "CREDIT_PURCHASE".credit_package_id
-where "CREDIT_PURCHASE".created_at >= '2024-11-01 00:00:00' and "CREDIT_PURCHASE".created_at <= '2024-11-30 23:59:59'
-group by "CREDIT_PACKAGE".name;
+
 -- 6-4. 查詢：計算 11 月份總營收（使用 purchase_at 欄位統計）
 -- 顯示須包含以下欄位： 總營收
-select sum(price_paid) as "總營收"
-from "CREDIT_PURCHASE"
-where "CREDIT_PURCHASE".purchase_at >= '2024-11-01 00:00:00' 
-and "CREDIT_PURCHASE".purchase_at <= '2024-11-30 23:59:59';
+
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
-select count(Distinct("COURSE_BOOKING".user_id)) as "預約會員人數"
-from "COURSE_BOOKING"
-where "COURSE_BOOKING".created_at >= '2024-11-01 00:00:00' 
-and "COURSE_BOOKING".created_at <= '2024-11-30 23:59:59' 
-and "COURSE_BOOKING".status != '課程已取消';
